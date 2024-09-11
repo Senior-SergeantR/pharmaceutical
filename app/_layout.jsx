2abb197478e1a6741bca17571923c2993d709f03
Senior-SergeantR <rabinotieno1@gmail.com>
2024-09-11 21:17:02 +0300

    added a navigation bar

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -30,6 +30,7 @@ const RootLayout = () => {
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)/Distributor" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)/Pharmacy" options={{ headerShown: false }} />
       <Stack.Screen name="(auth)" options={{ headerShown: false }} />
      
       {/* <Stack.Screen name="/search/[query]" options={{headerShown: false}}/> */}
