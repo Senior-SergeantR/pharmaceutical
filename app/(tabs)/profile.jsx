64bf3b35de3b1ddf6b0acfd675f40806a127e1cd
Senior-SergeantR <rabinotieno1@gmail.com>
2024-10-15 04:32:02 +0300

    minor change

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -59,7 +59,7 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   header: {
-    marginTop: 40,
+    marginTop: 0,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
